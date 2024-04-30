e78ecb626be9a70963b9ae22b3e88e84a9d2d78d
harshgvirtusa <156815642+harshgvirtusa@users.noreply.github.com>
2024-04-30 17:56:56 +0530

    Update WorkflowTemplate.py

--- WorkflowTemplate.py
+++ WorkflowTemplate.py
@@ -1,8 +1,8 @@
 from google.cloud import dataproc_v1 as dataproc
  
-project_id = "your-project-id"
+project_id = "dnb-genai-gcp-sandbox-207616"
 region = "us-central1"
-bucket_name = "your-bucket-name"
+bucket_name = "dataproc-bucket-sample"
  
 def create_simple_template(template_name):
     client_options = {"api_endpoint": f"{region}-dataproc.googleapis.com:443"}
@@ -10,16 +10,16 @@ def create_simple_template(template_name):
  
     template = dataproc.WorkflowTemplate()
     template.id = template_name
-    template.placement.cluster_selector.cluster_labels['env'] = "testtoday"
+    template.placement.cluster_selector.cluster_labels['env'] = "test"
  
     template.jobs = [
         {
             "pyspark_job": {
-                "main_python_file_uri": f"gs://{bucket_name}/path/to/your_script.py",
+                "main_python_file_uri": f"gs://{bucket_name}/pyspark_job.py", # path/to/your_script.py",
                 "args": [
-                    "glue1234",
-                    f"gs://{bucket_name}/path/to/input.csv",
-                    f"gs://{bucket_name}/path/to/output/"
+                    "pyspark_job",
+                    f"gs://{bucket_name}/customers.csv", # path/to/input.csv",
+                    f"gs://{bucket_name}/sample_output/" # path/to/output/"
                 ],
             },
             "step_id": f"{template_name}first",
